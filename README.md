7aa062f3ae6674b051fc2a7119da4edc71105a83
cherylnqj <cheryl.nqj@gmail.com>
2019-05-31 13:12:46 +0800

    Added reuseable button component and rendered CardSection components

--- README.md
+++ README.md
@@ -10,5 +10,7 @@ A quick project on learning to use react native by creating an albums applicatio
 * Functional Components vs Class-Based Components
 * Component Styling
 * Using Props to pass info from parent to child
-* Lifecycle hooks (componentDidMount)
+* Life-cycle hooks (componentDidMount)
 * Axios server requests, setStates and promises
+* Styling of objects with styles objects, flex-boxes
+* Component alignment and nesting
--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,11 +1,72 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, Text, Image, Linking } from 'react-native';
 import Card from './Card';
+import CardSection from './CardSection';
+import Button from './Button';
 
-const AlbumDetail = (props) => (
-    <Card>
-        <Text>{ props.album.title }</Text>
-    </Card>
-);
+const AlbumDetail = ({ album }) => {
+    const { title, artist, image, url } = album;
+    const { 
+        albumHeaderContentStyle, 
+        albumHeaderTextStyle, 
+        artistThumbnailStyle, 
+        thumbnailContainerStyle,
+        imageStyle } = styles;
+
+    return (
+        <Card>
+            <CardSection>
+                <View style={thumbnailContainerStyle}>
+                    <Image
+                        style={artistThumbnailStyle}
+                        source={{ uri: 'https://i.imgur.com/K3KJ3w4h.jpg' }} 
+                    />
+                </View>
+                <View style={albumHeaderContentStyle}>
+                        <Text style={albumHeaderTextStyle}>{ title }</Text>
+                        <Text>{ artist }</Text>
+                </View>
+            </CardSection>
+
+            <CardSection>
+                <Image 
+                    source={{ uri: image }} 
+                    style={imageStyle}
+                />
+            </CardSection>
+
+            <CardSection>
+                <Button onPress={() => Linking.openURL(url)}>
+                    Buy Now
+                </Button>
+            </CardSection>
+        </Card>
+    );
+};
+
+const styles = {
+    albumHeaderContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+    },
+    albumHeaderTextStyle: {
+        fontSize: 18,
+    },
+    artistThumbnailStyle: {
+        height: 50,
+        width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10,
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null,
+    }
+};
 
 export default AlbumDetail;
